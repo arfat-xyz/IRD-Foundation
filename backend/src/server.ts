065ecb02b96269c18ef72d61be81d67aef3f3120
unknown <arfatrahman08@gmail.com>
2025-05-15 22:48:47 +0600

    updated 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,18 @@
 import app from "./app";
 import { config } from "./config";
-import { Logger } from "./shared/logger";
+// import { Logger } from "./shared/logger";
 
 async function bootstrap() {
   try {
-    Logger.logger.info("Db is connected");
-    // console.log('Db is connected')
+    // Logger.logger.info("Db is connected");
+    console.log("Db is connected");
     app.listen(config.port, () => {
-      Logger.logger.info(`Application listening on port ${config.port}`);
-      // console.log(`Application listening on port ${config.port}`)
+      // Logger.logger.info(`Application listening on port ${config.port}`);
+      console.log(`Application listening on port ${config.port}`);
     });
   } catch (error) {
-    Logger.errorLogger.error(`Failed to connect database ${error}`);
-    // console.log(`Failed to connect database ${error}`)
+    // Logger.errorLogger.error(`Failed to connect database ${error}`);
+    console.log(`Failed to connect database ${error}`);
   }
 }
 bootstrap();
